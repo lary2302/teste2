b8a825c9da8881c81d31ee6697e5f63427dd0397
lary2302 <larissa.varela@escola.pr.gov.br>
2025-09-04 09:05:09 -0300

    ok

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const avanca = document.querySelectorA11('.btn-proximo');
 const reiniciarBtn = document.getElementById('.btn-reiniciar');
 
-
 avanca.forEach(button => {
+button.addEventListener('click',function(){
+    const atual = ducument.querySelector('.ativo');
+    const proximoPasso='passo-'+this.getAttribute('data-procimo');
+})
 
 
 }
