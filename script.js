adeb1faac6e330eca5e7db6c45c6cf4907434d57
lary2302 <larissa.varela@escola.pr.gov.br>
2025-09-11 08:25:48 -0300

    ok

--- script.js
+++ script.js
@@ -5,9 +5,32 @@ avanca.forEach(button => {
 button.addEventListener('click',function(){
     const atual = ducument.querySelector('.ativo');
     const proximoPasso='passo-'+this.getAttribute('data-procimo');
+    atual.classList.remove('ativo');
+    const proximoElemento=document.getElementById(proximoPasso);
+
+
+
+
+
+
+
+
+
+
+if(proximoElemento){
+    proximoElemento.classList.add('ativo');
+} else{
+    console.error(`Elemento com id "${proximoPasso}" não en
+
+    )
+}
+
+
+
+
+
+
 })
-
-
 }
 
 
