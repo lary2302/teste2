42e888a838b7a2e52982d5088d9d2491aaea76c9
lary2302 <larissa.varela@escola.pr.gov.br>
2025-09-11 09:06:43 -0300

    ok

--- script.js
+++ script.js
@@ -13,16 +13,14 @@ if(proximoElemento){
     proximoElemento.classList.add('ativo');
 } else{
     console.error(`Elemento com ID "${proximoPasso}" não encontrado.`)
-
 }
-
-
-
-
-
-
 })
+})
+// Reinicia o jogo ao clicar no botão de reinício
+if (reiniciarBtn) {
+    reiniciarBtn.addEventListener('click', () => {
+        const atual = document.querySelector('.ativo');
+        atual.classList.remove('ativo');
+        document.getElementById('passo-0').classList.add('ativo');
+    });
 }
-
-
-)
